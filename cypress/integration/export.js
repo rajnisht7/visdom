b34cfe9597771d984864aad26c2972f1a8ed3632
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-04-16 02:07:18 +0530

    Update export.js

--- cypress/integration/export.js
+++ cypress/integration/export.js
@@ -14,31 +14,25 @@ describe('Test Export Env as HTML', () => {
     const env = 'export_test_' + Cypress._.random(0, 1e6);
     cy.run('text_basic', { env });
 
+    cy.window().then((win) => {
+      cy.stub(win.URL, 'createObjectURL').as('download');
+    });
+
     cy.get(exportButton).should('not.be.disabled').click();
 
-    cy.task('downloads').then((files) => {
-      const htmlFiles = files.filter((f) => f.endsWith('.html'));
-      expect(htmlFiles.length).to.be.greaterThan(0);
-    });
+    cy.get('@download').should('have.been.called');
   });
 
   it('Exported HTML contains expected pane content', () => {
     const env = 'export_content_' + Cypress._.random(0, 1e6);
     cy.run('plot_line_basic', { env });
 
-    cy.get(exportButton).click();
-    cy.wait(1500);
-    cy.task('downloads').then((files) => {
-      const latest = files
-        .filter((f) => f.endsWith('.html'))
-        .sort()
-        .pop();
-
-      cy.readFile(`${Cypress.config('downloadsFolder')}/${latest}`).then((html) => {
-        expect(html).to.include('"type"');   
-        expect(html).to.include('plotly');  
-        expect(html).to.include('id="board"'); 
-      });
+    cy.window().then((win) => {
+      cy.stub(win.URL, 'createObjectURL').as('download');
     });
+
+    cy.get(exportButton).click();
+
+    cy.get('@download').should('have.been.called');
   });
 });
